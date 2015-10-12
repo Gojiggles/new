1d8921fd8be250fe39001066e31e3cd24941663b
Gojiggles <sabah.h2o@gmail.com>
2015-10-12 14:27:11 +0300

    Azaza

--- Bulbulator/Form1.Designer.cs
+++ Bulbulator/Form1.Designer.cs
@@ -301,7 +301,7 @@
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
             this.pictureBox1.Location = new System.Drawing.Point(315, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 46);
+            this.pictureBox1.Size = new System.Drawing.Size(54, 46);
             this.pictureBox1.TabIndex = 23;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
